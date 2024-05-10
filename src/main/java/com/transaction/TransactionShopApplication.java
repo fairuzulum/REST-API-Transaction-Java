081a9cfa13736708d0b8de3bb1b99c39469c96d2
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-05-10 10:16:13 +0700

    first commit

---
+++ src/main/java/com/transaction/TransactionShopApplication.java
@@ -0,0 +1,13 @@
+package com.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionShopApplication.class, args);
+	}
+
+}
