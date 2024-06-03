ea6bc94ddaf81a587b26a62be869e941547c89f8
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-06-03 19:17:43 +0700

    fix all project is finish alhamdulillah

--- src/main/java/com/transaction/dto/request/AuthRequest.java
+++ src/main/java/com/transaction/dto/request/AuthRequest.java
@@ -1,5 +1,6 @@
 package com.transaction.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -8,6 +9,9 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class AuthRequest {
+    @NotBlank(message = "username is required")
     private String username;
+
+    @NotBlank(message = "password is required")
     private String password;
 }
